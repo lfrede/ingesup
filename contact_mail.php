6747b1d4c52f786c90e20881e984b3082f206d4d
PallierJean <pallier.jean@hotmail.fr>
2014-10-20 11:50:31 +0200

    mail

---
+++ contact_mail.php
@@ -0,0 +1,63 @@
+<?php
+
+      $mail = $_POST['mail'];
+      $nom = $_POST['nom'];
+      $adresse = $_POST['adresse'];
+      $objet = $_POST['objet'];
+      $message = $_POST['message'];
+      $mail_dest = 'maviosha@hotmail.com'; // Déclaration de l'adresse de destination.
+
+      if (!preg_match("#^[a-z0-9._-]+@(hotmail|live|msn|outlook|free|orange|neuf|y-nov|gmail).[a-z]{2,4}$#", $mail)) // On filtre les serveurs qui rencontrent des bogues.
+      {
+            $passage_ligne = "\r\n";
+      } else {
+            $passage_ligne = "\n";
+      }
+      //=====Déclaration des messages au format texte et au format HTML.
+      //$message_txt = "Salut à tous, voici un e-mail envoyé par un script PHP.";
+      $message_html = "<html><head></head><body>Bonjour, </br></br> Voici le nom de la personne qui vous envoie le message : <i><b>".$nom."</b></i>, </br> et son adresse: <i><b>".$adresse."</b></i>, 
+      </br></br> objet: <i><b> ".$objet."</b></i> , </br></br> intitule du message: <b>\"".$message."\"</b></body></html>";
+      //==========
+                          
+      //=====Création de la boundary
+      $boundary = "-----=".md5(rand());
+      //==========
+                         
+      //=====Définition du sujet.
+      $sujet = "[".$nom."] Contact !";
+      //=========
+                          
+      //=====Création du header de l'e-mail.
+      $header = "From: ".$mail.$passage_ligne;
+      $header.= "To: ".$mail_dest.$passage_ligne;
+      $header.= "MIME-Version: 1.0".$passage_ligne;
+      $header.= "Content-Type: multipart/alternative;".$passage_ligne." boundary=\"$boundary\"".$passage_ligne;
+      //==========
+                          
+      //=====Création du message.
+      $message = $passage_ligne."--".$boundary.$passage_ligne;
+      //=====Ajout du message au format texte.
+      $message.= "Content-Type: text/plain; charset=\"UTF-8\"".$passage_ligne;
+      $message.= "Content-Transfer-Encoding: 8bit".$passage_ligne;
+      $message.= $passage_ligne.$message_txt.$passage_ligne;
+      //==========
+      $message.= $passage_ligne."--".$boundary.$passage_ligne;
+      //=====Ajout du message au format HTML
+      $message.= "Content-Type: text/html; charset=\"UTF-8\"".$passage_ligne;
+      $message.= "Content-Transfer-Encoding: 8bit".$passage_ligne;
+      $message.= $passage_ligne.$message_html.$passage_ligne;
+      //==========
+      $message.= $passage_ligne."--".$boundary."--".$passage_ligne;
+      $message.= $passage_ligne."--".$boundary."--".$passage_ligne;
+      //==========
+                          
+      //=====Envoi de l'e-mail.
+      mail($mail,$sujet,$message,$header);
+      //==========
+
+      //rederection
+      header('Location: Ecuries_contacts.php');
+?>
+
+
+
